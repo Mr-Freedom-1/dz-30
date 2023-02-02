603fdc63348f06f3e39aa5197e9f1f0757916ab7
Mr-Freedom-1 <glebpoterailo@gmail.com>
2023-02-02 17:45:21 +0200

    first commit

---
+++ js/script.test.js
@@ -0,0 +1,45 @@
+/*-------------Завдання №1-------------*/
+// describe("describe", function() {
+//     it('test', function() {
+
+//     });
+//   })
+
+let assert = chai.assert;
+describe("validateEmail", function() {
+    describe('valid string', function(){
+        it('not an empty string', function() {
+            assert.equal(validateEmail(''), false);
+        });
+        it('typeof string', function() {
+            assert.equal(validateEmail(222), false, 'number error');
+            assert.equal(validateEmail(true), false, 'boolean error');
+            assert.equal(validateEmail({}), false, 'object error');
+        });
+        it('not an empty string', function() {
+            assert.equal(validateEmail('     '), false);
+        });
+    })
+    describe('valid data', function(){
+        it('Don\'t have the @ or @ the first character', function(){
+            assert.isFalse(validateEmail('emailgmail.com'), 'Don\'t have the @');
+            assert.isFalse(validateEmail('@emailgmail.com'), '@ the first character')
+        })
+
+        /*-------------Завдання №2-------------*/
+        it('Less than 5 characters', function(){
+            assert.isFalse(validateEmail('@gmail'), 'More than 4')
+        })
+
+        /*-------------Завдання №3-------------*/
+        it('Has more than one @', function(){
+            assert.isFalse(validateEmail('email@gmail@.com'), 'More than one @')
+        })
+
+        /*-------------Завдання №4-------------*/
+        it('Dot comes before @ or dot does not come after @', function(){
+            assert.isFalse(validateEmail('email@gmailcom'), 'No dot after @');
+            assert.isFalse(validateEmail('email.@gmail.com'), 'Dot just before @')
+        })
+    })
+})
